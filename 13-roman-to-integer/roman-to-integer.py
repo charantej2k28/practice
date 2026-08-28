bbeb7e9e53ce8b33de55199391b84eac33bfa064
charantej2k28 <charangonnakuti7@gmail.com>
2026-08-28 17:11:11 +0530

    Time: 18 ms (12.20%) | Memory: 12.3 MB (57.86%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+            val = roman_map[s[i]]
+            if i + 1 < n and val < roman_map[s[i + 1]]:
+                total -= val
+            else:
+                total += val
+                
+        return total
