a96665912a159636e2a2acf0e62a8fa4689a9216
charantej2k28 <charangonnakuti7@gmail.com>
2026-09-01 18:50:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (64.05%) - LeetSync

---
+++ 50-powx-n/powx-n.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n < 0:
+            x = 1.0 / x
+            n = -n
+            
+        result = 1.0
+        current_product = x
+        
+        while n > 0:
+            if n % 2 == 1:
+                result *= current_product
+            current_product *= current_product
+            n //= 2
+            
+        return result
