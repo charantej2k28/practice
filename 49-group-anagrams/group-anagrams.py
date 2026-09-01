4213aebb4d939061b66abc44e2a77b79c768f6ed
charantej2k28 <charangonnakuti7@gmail.com>
2026-09-01 18:50:06 +0530

    Time: 15 ms (95.74%) | Memory: 16.9 MB (23.13%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        anagram_map = defaultdict(list)
+        
+        for s in strs:
+            key = tuple(sorted(s))
+            anagram_map[key].append(s)
+            
+        return list(anagram_map.values())
