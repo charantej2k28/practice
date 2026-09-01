18a5b3718927756ea657c557d36e2a41fcdf58de
charantej2k28 <charangonnakuti7@gmail.com>
2026-09-01 18:42:46 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (22.27%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        
+        def backtrack(start):
+            if start == len(nums):
+                res.append(list(nums))
+                return
+            for i in range(start, len(nums)):
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start + 1)
+                nums[start], nums[i] = nums[i], nums[start]
+                
+        backtrack(0)
+        return res
