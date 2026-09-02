ad445a3daf73a2ffde1ccbc1d3825812c709cdc7
charantej2k28 <charangonnakuti7@gmail.com>
2026-09-02 21:46:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (19.51%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        if not matrix or not matrix[0]:
+            return []
+
+        res = []
+        top, bottom = 0, len(matrix) - 1
+        left, right = 0, len(matrix[0]) - 1
+
+        while top <= bottom and left <= right:
+            # Traverse from left to right along top boundary
+            for col in range(left, right + 1):
+                res.append(matrix[top][col])
+            top += 1
+
+            # Traverse downwards along right boundary
+            for row in range(top, bottom + 1):
+                res.append(matrix[row][right])
+            right -= 1
+
+            # Check if row boundary is still valid
+            if top <= bottom:
+                # Traverse from right to left along bottom boundary
+                for col in range(right, left - 1, -1):
+                    res.append(matrix[bottom][col])
+                bottom -= 1
+
+            # Check if column boundary is still valid
+            if left <= right:
+                # Traverse upwards along left boundary
+                for row in range(bottom, top - 1, -1):
+                    res.append(matrix[row][left])
+                left += 1
+
+        return res
